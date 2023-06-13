ef95de2c568becc797681486acd4773799b90186
Tara Denniston <taradenniston@gmail.com>
2023-06-13 09:58:54 -0400

    add form for editing profile

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Optional, Email, Length
 
 
 class MessageForm(FlaskForm):
@@ -16,6 +16,16 @@ class UserAddForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
     image_url = StringField('(Optional) Image URL')
+
+class UserEditForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username')
+    email = StringField('E-mail', validators=[Optional(), Email()])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Header Image URL')
+    bio = TextAreaField('Bio')
+    password = PasswordField('Password')
 
 
 class LoginForm(FlaskForm):
